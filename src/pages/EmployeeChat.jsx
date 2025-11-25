127aa9ef2f4e3345a4dfd3d0051cafe98322f52a
moses okonkwo <okonkwomoses158@gmail.com>
2025-11-25 09:06:17 +0100

    added scrollable chat system

--- src/pages/EmployeeChat.jsx
+++ src/pages/EmployeeChat.jsx
@@ -214,7 +214,7 @@ export default function EmployeeChat() {
           </CardHeader>
           <CardContent className="flex-1 flex flex-col p-0">
             {/* Messages */}
-            <div className="flex-1 overflow-y-auto p-4 space-y-4">
+            <div className="flex-1 overflow-y-auto p-4 space-y-4 min-h-0">
               {messages.length === 0 ? (
                 <div className="flex items-center justify-center h-full text-gray-500 dark:text-gray-400">
                   <div className="text-center">
